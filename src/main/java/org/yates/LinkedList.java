c0c54da0b352069c2b54a488d41e50a86d6ee862
YatesRocks <yates.j.a@outlook.com>
2023-11-13 23:49:56 -0500

    yay finished

--- src/main/java/org/yates/LinkedList.java
+++ src/main/java/org/yates/LinkedList.java
@@ -1,2 +1,233 @@
-package org.yates;public class LinkedList {
+package org.yates;
+
+import java.util.*;
+
+public class LinkedList<T> implements Iterable<T> {
+    /* The element of header will never be accessed
+     * The "real" head will be header.next, the tail is header.prev */
+    private Node<T> header = new Node<T>(null, null, null);
+    private int size = 0;
+
+    public LinkedList() {
+        header.prev = header.next = header;
+    }
+
+    public LinkedList(Collection<? extends T> c) {
+        this();
+        addAll(c);
+    }
+
+    public LinkedList(T[] c) {
+        this();
+        addAll(size, c);
+    }
+
+    // -- + -- +-> Private Methods <-+ -- + --
+
+    /* Inserts a new node with element "e" before
+     * the given Node "n" (adds e before n) */
+    private Node<T> addBefore(T e, Node<T> n) {
+        Node<T> nN = new Node<T>(n.prev, e, n);
+        n.prev.next = nN;
+        n.prev = nN;
+        size++;
+        return nN;
+    }
+
+    /* Given an index within the bounds of the arr,
+     * returns a pointer to the Node at that index */
+    private Node<T> elementAt(int index) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException(
+                    "Size: " + size + " Index: " + index
+            );
+
+        Node<T> cursor = header;
+        if (index > size >> 1) {
+            for (int i=size; i>index; i--)
+                cursor = cursor.prev;
+        } else {
+            for (int i=0; i<=index; i++)
+                cursor = cursor.next;
+        }
+
+        return cursor;
+    }
+
+    // -- + -- +-> Add Methods <-+ -- + --
+
+    public void addFirst(T e) {
+        addBefore(e, header.next);
+    }
+
+    public void add(int index, T e) {
+        addBefore(e, elementAt(index));
+    }
+
+    public void add(T e) {
+        addBefore(e, header);
+    }
+
+    public boolean addAll(int index, T[] aArr) {
+        int len = aArr.length;
+        if (len == 0) return false;
+        Node<T> successor = elementAt(index).next;
+        Node<T> predecessor = successor.prev;
+        for (int i=0; i<len; i++) {
+            Node<T> n = new Node<T>(predecessor, aArr[i], successor);
+            n.next.prev = n;
+            n.prev.next = n;
+            predecessor = n;
+        }
+        successor.prev = predecessor;
+        size += len;
+        return true;
+    }
+
+    public boolean addAll(int index, Collection<? extends T> a) {
+        T[] aArr = (T[]) a.toArray();
+        return addAll(index, aArr);
+    }
+
+    public boolean addAll(Collection<? extends T> a) {
+        return addAll(size, a);
+    }
+
+    // -- + -- +-> Remove Method <-+ -- + --
+
+    /* In order for the GC to free memory,
+     * all existing pointers to elem at index
+     * must be removed. */
+    public T remove(int index) {
+        Node<T> temp = elementAt(index);
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
+        temp.prev = temp.next = null;
+        size--;
+        return temp.elem;
+    }
+
+    public void clear() {
+        int n = size;
+        for (int i = 0; i < n; i++)
+            remove(0);
+    }
+
+    // -- + -- +-> Get Methods <-+ -- + --
+
+    public T get(int index) {
+        return elementAt(index).elem;
+    }
+
+    public T get() {
+        return header.prev.elem;
+    }
+
+    public T getFirst() {
+        return header.next.elem;
+    }
+
+    // -- + -- +-> Set Methods <-+ -- + --
+
+    public T set(int index, T e) {
+        return addBefore(e, elementAt(index)).elem;
+    }
+
+    // -- + -- +-> Find Methods <-+ -- + --
+
+    public int indexOf(Object o) {
+        int i = 0;
+        if (o == null) {
+            for (Node n = header.next; n != header; n = n.next) {
+                if (n.elem == null)
+                    return i;
+                i++;
+            }
+        } else {
+            for (Node n = header.next; n != header; n = n.next) {
+                if (o.equals(n.elem))
+                    return i;
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    // -- + -- +-> Misc <-+ -- + --
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public String toString() {
+        String buf = new String();
+        if (size == 0) return "[]";
+        buf += "[";
+        for (Node n=header.next; n != header; n = n.next)
+            buf += " " + n.elem + ",";
+        buf += "\b ]";
+        return buf;
+    }
+
+    public Object[] toArray() {
+        Object[] arr = new Object[size];
+        Node<T> cursor = header.next;
+        for (int i = 0; i < size; i++) {
+            arr[i] = cursor.elem;
+            cursor = cursor.next;
+        }
+        return arr;
+    }
+
+    // -- + -- +-> Node <-+ -- + -- 
+
+    /* Static allows instantiation of Node
+     * without instantiation of LinkedList.
+     * Node is a nested private class in order
+     * to lower redundant function calls because
+     * they have overhead that adds up w/frequent
+     * accesses. */
+    private static class Node<T> {
+        Node<T> prev;
+        T elem;
+        Node<T> next;
+
+        Node(Node<T> prev, T elem, Node<T> next) {
+            this.prev = prev;
+            this.elem = elem;
+            this.next = next;
+        }
+    }
+
+    // -- + -- +-> Iterator Interface <-+ -- + --
+
+    // NOTE: Broken <-------- TODO: Fix
+
+    public Iterator<T> iterator() {
+        return new Iter<T>();
+    }
+
+    private class Iter<E> implements Iterator<E> {
+        // Safety: E is equivalent to T as it's only called from
+        // the context of T
+        private Node<E> cursor = (Node<E>) header;
+
+        public boolean hasNext() {
+            return cursor != header;
+        }
+
+        public E next() {
+            Node<E> res = cursor;
+            cursor = cursor.next;
+            return res.elem;
+        }
+    }
 }
--- src/main/java/org/yates/Main.java
+++ src/main/java/org/yates/Main.java
@@ -1,2 +1,189 @@
-package org.yates;public class Main {
+package org.yates;
+
+// Disclaimer: This code was in part from a project I did a few years ago for AP CS A, found online at https://replit.com/@JordanYates1
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class CLI {
+    private static char esc = (char) 27;
+    private static char b = '[';
+    private static String escb = esc + "[";
+    private static String clear = escb + "2J";
+    private static String reset = escb + "0m";
+    private static String bold = escb + "1m";
+
+    private static String red = reset + escb + "31m";
+    private static String orange = reset + escb + "37m";
+    private static String yellow = reset + escb + "33m";
+
+    private static String green = reset + escb + "32m";
+
+    private static String blue = reset + escb + "34m";
+    private static String indigo = reset + escb + "36m";
+    private static String violet = reset + escb + "35m";
+
+    private static final String choiceMsg = blue + bold + "\nChoose an option from the list below:\n";
+    private static String menu = choiceMsg + blue + "\t1) Add\n" + red + "\t2) Set\n\t3) Find\n" + blue + "\t4) Remove\n\t5) Clear\n\t6) Quit\n";
+    private static String addSubmenu = choiceMsg + blue + "\t1) Add to end\n\t2) Add at index\n\t3) Added to front\n"+ red +"\t4) Add all\n"+blue+"\t5) Back";
+    private static String removeSubmenu = choiceMsg + blue + "\t1) Remove from end\n\t2) Remove at index\n\t3) remove from front\n\t4) Remove all\n\t5) Back";
+
+
+    private static Scanner sca = new Scanner(System.in);
+
+    private final String inputMsg = reset + bold + "Please, put your input below\n" + reset + "λ:";
+
+    private static final String index = "\n -- INDEX -- \n";
+
+    private String[] st = {"This", "is", "your", "list"};
+    private LinkedList<String> strList = new LinkedList<>(st);
+
+    public void header() {
+        System.out.print(clear);
+        System.out.println(red + " █ - "
+                + orange + "█ - "
+                + yellow + "█ - "
+                + green + bold + "Linked-List CLI " + reset
+                + blue + "- █ - "
+                + indigo + "█ - "
+                + violet + "█\n"
+        );
+        System.out.println(strList);
+    }
+
+    private String getString() {
+        System.out.print(inputMsg);
+        return sca.nextLine();
+    }
+
+    private int getInt() {
+        System.out.print(inputMsg);
+        int result;
+        try {
+            result = (int) Integer.parseInt(sca.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println(red + bold + "Whoops, ensure your input is correct." + reset);
+            result = getInt();
+        }
+        return result;
+    }
+
+    private void append() {
+        header();
+        strList.add(getString());
+    }
+
+    private void addIndex() {
+        header();
+        System.out.println(index);
+        int index = getInt();
+        header();
+        System.out.println();
+        strList.add(index, getString());
+    }
+
+    private void addFront() {
+        header();
+        strList.add(0, getString());
+    }
+
+    private void removeFront() {
+        header();
+        strList.remove(0);
+    }
+
+    private void removeLast() {
+        header();
+        strList.remove(strList.size() - 1);
+    }
+
+    private void removeIndex() {
+        header();
+        System.out.println(index);
+        int index = getInt();
+        header();
+        System.out.println();
+        strList.remove(index);
+    }
+
+    private static final String addListMenu = choiceMsg + blue + "\t1) Add another item to the temp list\n\t2) Finish";
+
+    private void addList() {
+        ArrayList<String> arr = new ArrayList<>();
+        int index = 0;
+        while (true) {
+            switch (getInt()) {
+                case 1: break;
+                case 2: strList.addAll(arr); return;
+            }
+        }
+    }
+
+    private void removeSubmenu() {
+        while (true) {
+            header();
+            System.out.println(removeSubmenu);
+            switch (getInt()) {
+                case 1: removeLast(); break;
+                case 2: removeIndex(); break;
+                case 3: removeFront(); break;
+                case 4: strList.clear(); break;
+                case 5: return;
+            }
+        }
+    }
+
+    private void addSubmenu() {
+        while (true) {
+            header();
+            System.out.println(addSubmenu);
+            switch (getInt()) {
+                case 1: append(); break;
+                case 2: addIndex(); break;
+                case 3: addFront(); break;
+                case 4: addList(); break;
+                case 5: return;
+            }
+        }
+    }
+
+    private void mainMenu() {
+        while (true) {
+            header();
+            System.out.println(menu);
+            switch (getInt()) {
+                case 1: addSubmenu(); break;
+                case 2: break;
+                case 3: break;
+                case 4: removeSubmenu();
+                case 5: strList.clear(); break;
+                case 6: if (ynconfirm()) { return; } else { break; }
+            }
+        }
+    }
+
+    private boolean ynconfirm() {
+        header();
+        while (true) {
+            System.out.println(blue + "Are you sure you would like to quit?\n\t1)" + red + " Yes\n\t" + blue + "2) No");
+            switch (getInt()) {
+                case 1: return true;
+                case 2: return false;
+                default: System.out.println("Choose either 1 (Yes) or 2 (No)"); break;
+            }
+        }
+
+    }
+
+    public void start() {
+        mainMenu();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // --- CLI
+        CLI cli = new CLI();
+        cli.start();
+    }
 }
